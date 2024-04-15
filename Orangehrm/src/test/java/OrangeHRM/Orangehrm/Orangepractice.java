533619deb43aa2319b5290970b6a3ed61a25df2a
Ahmed Bilal <ahmed.bilal@wearenova.co.uk>
2024-04-15 18:50:54 +0500

    Test commit by Ahmed Bilal

---
+++ Orangehrm/src/test/java/OrangeHRM/Orangehrm/Orangepractice.java
@@ -0,0 +1,92 @@
+package OrangeHRM.Orangehrm;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class Orangepractice {
+
+	WebDriver driver;
+
+	@BeforeTest
+	public void beforeTest() {
+
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	}
+
+	@AfterTest
+	public void afterTest() {
+
+		driver.quit();
+	}
+
+	@Test(priority=1)
+	public void OrangeLoginFailure() throws InterruptedException {
+
+		Thread.sleep(3000);
+
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Adminn");
+
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+
+		Thread.sleep(3000);
+
+		String Failure_text =driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]")).getText();
+
+		System.out.println(Failure_text);
+
+		String Expectedfailuretext="Invalid credentials";
+
+		Assert.assertEquals(Failure_text, Expectedfailuretext);
+		
+		Thread.sleep(5000);
+
+	}
+
+	@Test(priority=2)
+	public void OrangeLoginSuccess() throws InterruptedException {
+
+
+		Thread.sleep(3000);
+
+		String login_text=driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/h5")).getText();
+
+		System.out.println(login_text);
+
+		String home_logo_alt = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[1]/img")).getAttribute("alt");
+
+		System.out.println(home_logo_alt);
+
+		String Expectedlogotext="company-branding";
+
+		Assert.assertEquals(home_logo_alt, Expectedlogotext);
+
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admin");
+
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123");
+
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+		System.out.println("We are logged-in!");
+
+		Thread.sleep(5000);
+
+
+	}
+
+	
+	
+	
+	
+}
